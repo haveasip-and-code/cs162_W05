72d83e9e92c4f03abcae10e12c3e7dd677544dca
Nguyễn Bạch Trường Giang <22125020@student.hcmus.edu.vn>
2023-03-16 13:06:43 +0700

    update main

--- main.cpp
+++ main.cpp
@@ -6,32 +6,55 @@ struct listNode
 {
 	int val;
 	listNode *next;
-	listNode(int x)
-	{
-		val = x;
-		next = nullptr;
-	}
+
 };
-void input(node*& head)
+
+bool detectCycle(listNode *head)
+{
+    if (head==nullptr) return 0;
+    
+    listNode *i = head;
+    listNode *j = head;
+    if(head->next == nullptr) return false;
+    
+    j = j->next;
+    while(i != j && i != nullptr && j != nullptr && j->next != nullptr)
+    {
+        cerr << i->val << " " << j->val << "\n";
+        i = i->next;
+        j = j->next->next;
+    }
+    
+    if(i == j) return true;
+    return false;
+}
+
+void input(listNode*& head)
 {
     int n;
-    inp >> n;
+    cin >> n;
     if (n==0) return;
     
-    node* cur = head;
+    listNode* cur = head;
+    listNode* last;
     while (n)
     {
-        cur->data = n;
-        cur->next = new node;
+        last = cur;
+        cur->val = n;
+        cur->next = new listNode;
         cur = cur->next;
         cur->next = nullptr;
-        inp >> n;
+        cin >> n;
     }
+    last->next = head->next;
 }
 
 signed main()
 {
-	listNode *head = nullptr;
+	listNode *head = new listNode;
 	input(head);
+    
+    if(detectCycle(head)) cout << "there is a cycle\n";
+    else cout << "There is not a cycle\n";
 	return 0;
 }
